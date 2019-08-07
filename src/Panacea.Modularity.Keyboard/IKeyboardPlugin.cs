6628003ccce0717d5f4e1e48814674e2baf16be5
Portno <j.remoundos@dotbydot.gr>
2019-08-07 12:15:34 +0300

    first commit

---
+++ src/Panacea.Modularity.Keyboard/IKeyboardPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Keyboard
+{
+    public interface IKeyboardPlugin:IPlugin
+    {
+        void ShowKeyboard(KeyboardType type);
+
+        void HideKeyboard();
+    }
+}
---
+++ src/Panacea.Modularity.Keyboard/PanaceaServicesextensions.cs
@@ -0,0 +1,18 @@
+﻿using Panacea.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Keyboard
+{
+    public static class PanaceaServicesextensions
+    {
+        public static bool TryGetKeyboard(this PanaceaServices core, out IKeyboardPlugin keyboard)
+        {
+            keyboard = core.PluginLoader.GetPlugins<IKeyboardPlugin>().FirstOrDefault();
+            return keyboard != null;
+        }
+    }
+}
